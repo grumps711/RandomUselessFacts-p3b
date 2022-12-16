435a2e484a1106794baf4f154e169ecd71db2d1c
jvictorrodriguez <vrodriguezbarbeira@gmail.com>
2022-12-16 15:31:45 +0100

    15/12/22

--- src/main/java/com/ironhack/t3_ve_ruf/T3VeRufApplication.java
+++ src/main/java/com/ironhack/t3_ve_ruf/T3VeRufApplication.java
@@ -1,14 +1,28 @@
 package com.ironhack.t3_ve_ruf;
 
+import com.ironhack.t3_ve_ruf.menu.Menu;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class T3VeRufApplication {
+@RequiredArgsConstructor
+public class T3VeRufApplication implements CommandLineRunner {
+    private final Menu menu;
 
     public static void main(String[] args) {
         SpringApplication.run(T3VeRufApplication.class, args);
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        menu.startApp();
+
+
     }
 }
--- src/main/java/com/ironhack/t3_ve_ruf/dto/FactDTO.java
+++ src/main/java/com/ironhack/t3_ve_ruf/dto/FactDTO.java
@@ -2,13 +2,18 @@ package com.ironhack.t3_ve_ruf.dto;
 
 import com.ironhack.t3_ve_ruf.model.Fact;
 import jakarta.persistence.Version;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.Instant;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FactDTO {
 
     private String text;
