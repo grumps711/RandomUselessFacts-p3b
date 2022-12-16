6a18ed19d8810a146051ffa7ca57012af41287e7
jvictorrodriguez <vrodriguezbarbeira@gmail.com>
2022-12-16 15:54:23 +0100

    Begining of ConsoleInteracction

--- src/main/java/com/ironhack/t3_ve_ruf/menu/Menu.java
+++ src/main/java/com/ironhack/t3_ve_ruf/menu/Menu.java
@@ -4,6 +4,8 @@ import com.ironhack.t3_ve_ruf.controller.FactController;
 import com.ironhack.t3_ve_ruf.dto.FactDTO;
 import com.ironhack.t3_ve_ruf.proxy.FactProxy;
 import com.ironhack.t3_ve_ruf.service.FactService;
+import com.ironhack.t3_ve_ruf.utils.ConsoleColors;
+import com.ironhack.t3_ve_ruf.utils.ConsoleInteracction;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,15 +24,48 @@ private final FactProxy factProxy;
 
 
 
-    public void startApp(){
+    public void startApp() {
 
-        System.out.println("Aquí empieza tu app");
+        for (int i = 0; i < 30; i++) {
 
+            System.out.println();
+        }
+        System.out.println(ConsoleColors.BLUE_BACKGROUND + "Aquí empieza tu app" + ConsoleColors.RESET);
 
+        showMenu();
 
         System.out.println(factProxy.getOne());
 
-        //var dto = new FactDTO(new FactService());
+    }
+        private void showMenu() {
+            var input = "";
+            while (!input.equalsIgnoreCase("EXIT")) {
+
+                System.out.println(":::: Hello Task Manager ::::");
+
+                System.out.println("""
+                                -Show a Random Fact
+                                -Add to Favorites
+                                -Delete from Favorites
+                                -Delete All
+                                
+                                EXIT  To quit the program
+                                            
+                        """);
+                input = ConsoleInteracction.getStringInput();
+
+                if (input.equalsIgnoreCase("Show a Random Fact")) {
+                    System.out.println("Show a Random Fact");
+                    //todo call the method
+                } else if (input.equalsIgnoreCase("Add to Favorites")) {
+                    System.out.println("Add to Favorites");
+                    //todo call the method
+                }
+            }
+            System.exit(0);
+
+
+            //var dto = new FactDTO(new FactService());
 
 
     }
